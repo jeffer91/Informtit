4891bd7cca1c7bde0d89d8a662d2f225741061f6
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-08-06 16:49:47 -0500

    feat: cargar organización paginada de núcleos

--- static/completion-ui.js
+++ static/completion-ui.js
@@ -1,6 +1,12 @@
 (() => {
   const script = document.createElement('script');
-  script.src = '/completion-ui-v2.js?v=1.6';
+  script.src = '/completion-ui-v2.js?v=1.7';
   script.defer = true;
+  script.onload = () => {
+    const organization = document.createElement('script');
+    organization.src = '/nuclei-organization.js?v=1.7';
+    organization.defer = true;
+    document.head.appendChild(organization);
+  };
   document.head.appendChild(script);
 })();
