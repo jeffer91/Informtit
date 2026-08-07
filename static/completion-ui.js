b1efd0a35cebf541234535b62e6f1ed698be7832
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-08-07 14:48:25 -0500

    refactor: desacoplar complexivo de requisitos y núcleos

--- static/completion-ui.js
+++ static/completion-ui.js
@@ -1,12 +1,6 @@
 (() => {
   const completion = document.createElement('script');
-  completion.src = '/completion-ui-v2.js?v=2.5';
+  completion.src = '/completion-ui-v2.js?v=3.0';
   completion.defer = true;
-  completion.onload = () => {
-    const workflow = document.createElement('script');
-    workflow.src = '/workflow-ui.js?v=2.5';
-    workflow.defer = true;
-    document.head.appendChild(workflow);
-  };
   document.head.appendChild(completion);
 })();
